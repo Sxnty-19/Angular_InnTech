18d7e4c90f879e6239c21cab793577b4f10612d1
7DiegoVa7 <diegovasquezarteaga5@gmail.com>
2026-02-19 00:07:27 -0500

    Add (Portada)

--- src/app/pages/inicio/inicio.ts
+++ src/app/pages/inicio/inicio.ts
@@ -1,30 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Footer } from '../../components/footer/footer';
-import { Navbar } from '../../components/navbar/navbar';
-
+import { RouterLink } from '@angular/router';
+import { Footer } from '../../components/footer/footer'; // Ajusta la ruta si es necesario
 @Component({
   selector: 'app-inicio',
-  imports: [CommonModule, Footer, Navbar],
+  standalone: true,
+  imports: [CommonModule, RouterLink, Footer],
   templateUrl: './inicio.html',
-  styleUrl: './inicio.css',
+  styleUrls: ['./inicio.css']
 })
 export class Inicio {
-    habitaciones = [
+  habitaciones = [
     {
-      nombre: 'Habitación Individual',
-      descripcion: 'Perfecta para viajeros solitarios.',
-      imagen: 'img/habitaciones/h1.jpg'
+      nombre: "Habitación Sencilla",
+      descripcion: "Diseñada para el viajero solitario, ofrece comodidad y privacidad con una cama individual grande.",
+      imagen: "/habitaciones/sencilla.png",
     },
     {
-      nombre: 'Habitación Doble',
-      descripcion: 'Ideal para parejas o amigos.',
-      imagen: 'img/habitaciones/h2.jpg'
+      nombre: "Habitación Doble",
+      descripcion: "Perfecta para parejas o dos amigos. Disponible con una cama doble grande o dos camas individuales.",
+      imagen: "/habitaciones/doble.png",
     },
     {
-      nombre: 'Habitación Familiar',
-      descripcion: 'Espacio amplio para toda la familia.',
-      imagen: 'img/habitaciones/h3.jpg'
-    }
+      nombre: "Habitación Múltiple",
+      descripcion: "Ideal para grupos pequeños o familias. Ofrece tres o cuatro camas individuales cómodas.",
+      imagen: "/habitaciones/multiple.png",
+    },
+    {
+      nombre: "Habitación Quíntuple",
+      descripcion: "Gran espacio con cinco camas, pensada para equipos deportivos o grandes grupos de amigos.",
+      imagen: "/habitaciones/quintuple.png",
+    },
+    {
+      nombre: "Habitación Séxtuple",
+      descripcion: "Nuestra opción más espaciosa, con seis camas. Máxima capacidad y excelente para delegaciones.",
+      imagen: "/habitaciones/sextuple.png",
+    },
   ];
 }
